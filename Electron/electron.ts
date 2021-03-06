68cade3f6b30089cdce1b81d9c7b12855444b133
CavasCallahan <diogoassuncao12346@hotmail.com>
2021-03-06 19:46:10 +0000

    Initial Commit

---
+++ Electron/electron.ts
@@ -0,0 +1,36 @@
+import { BrowserWindow, app } from 'electron'
+import * as path from 'path'
+import * as url from 'url'
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '/../build/index.html'),
+    protocol: 'file:',
+    slashes: true
+});
+  win.loadURL(startUrl);
+
+  win.loadURL('http://localhost:3000/')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
